4964a2216cbf31318c175a20123389392af481fc
Kathryn Sherazi <67169418+kecoston@users.noreply.github.com>
2020-11-16 18:48:43 -0600

    exploring layout and connecting json through props

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,20 @@
-import Title from './components/Title';
-import Employee from './components/Employee';
-import Search from './components/Search'
+import Title from './components/Title/Title';
+import Employee from './components/Employee/Employee';
+import Search from './components/Search/Search'
+import employees from "./employee-list.json"
 
 function App() {
   return (
     <div className="container">
     <Title/>
     <Search/>
-    <Employee/>
+    <Employee
+    image={employees[0].image}
+    firstName={employees[0].firstName}
+    lastName={employees[0].lastName}
+    role={employees[0].role}
+    department={employees[0].department}
+    />
     </div>
   )
 }
---
+++ employee-directory/src/components/Employee/Employee.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./employee.css"
+
+function Employee(props) {
+    return (
+        <div className="card mb-3" className="row">
+           
+            <div className="col-md-2">
+            <img src={props.image} className="card-img" alt={props.name}></img>
+            </div>
+
+            <div className="col-md-9">
+            <h2 className="card-title">Name: {props.firstName} {props.lastName}</h2>
+            <h3 className="card-text"> Role: {props.role} | Department: {props.department}</h3>
+        
+            </div>
+
+        </div>
+    )
+}
+
+export default Employee
